bd371bee6faf2c0fc683fdb114db0c742cb4bbf0
Cctrs <126019611+Cctrs@users.noreply.github.com>
2023-03-16 18:57:51 +0300

    Update first_flask_exp.py

--- first_flask_exp.py
+++ first_flask_exp.py
@@ -8,3 +8,9 @@ def all_methods():
     future_possibilities = ['/post_my_wisdom', '/delete_my_humble_opinion', '/get_Statham_confidence']
     return future_possibilities
 
+
+@app.route('/post_my_wisdom', methods=['POST'])
+def post_my_wisdom():
+    great_text = request.form.get('text')
+    answer = 'Someone really wise said long time ago that: "' + great_text +'"'
+    return answer
